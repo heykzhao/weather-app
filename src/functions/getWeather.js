44a04878c5e4410f988913f6cccae9d00c1d135f
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-01-19 17:31:27 -0500

    finalize what data to pull from API

--- src/functions/getWeather.js
+++ src/functions/getWeather.js
@@ -14,18 +14,33 @@ async function getCurrentCity(city) {
   }
 }
 
-async function getWeatherArray(obj) {
+async function getWeatherArrayHourly(obj) {
   const weatherArray = [];
   for (let i = 0; i < obj.length; i += 1) {
     const arrayItem = {
-      hour: obj[i].dt,
+      date: new Date(obj[i].dt * 1000).getHours(),
+      temperature: obj[i].temp,
+      description: obj[i].weather[0].main,
     };
     weatherArray.push(arrayItem);
   }
   return weatherArray;
 }
 
-export default async function getCurrentCityWeather(city, units) {
+async function getWeatherArrayDaily(obj) {
+  const weatherArray = [];
+  for (let i = 0; i < obj.length; i += 1) {
+    const arrayItem = {
+      date: new Date(obj[i].dt * 1000).getDay(),
+      minTemperature: obj[i].temp.min,
+      maxTemperature: obj[i].temp.max,
+    };
+    weatherArray.push(arrayItem);
+  }
+  return weatherArray;
+}
+
+export async function getCurrentCityWeather(city, units) {
   try {
     const currentCityCoord = await getCurrentCity(city);
     const { lat, lon } = currentCityCoord;
@@ -45,9 +60,42 @@ export default async function getCurrentCityWeather(city, units) {
       currentDetails: current.weather.description,
     };
 
-    const hourlyWeather = await getWeatherArray(hourly);
+    const hourlyWeather = await getWeatherArrayHourly(hourly);
 
-    const dailyWeather = await getWeatherArray(daily);
+    const dailyWeather = await getWeatherArrayDaily(daily);
+
+    const allWeather = {
+      currentWeather,
+      hourlyWeather,
+      dailyWeather,
+    };
+
+    return allWeather;
+  } catch (error) {
+    return console.log(error);
+  }
+}
+
+export async function getCurrentCoordinatesWeather(lat, lon, units) {
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&exclude=minutely&appid=${APIKey}`);
+    const data = await response.json();
+
+    const { current, hourly, daily } = data;
+
+    const currentWeather = {
+      currentTime: new Date(current.dt * 1000),
+      currentTemp: current.temp,
+      currentFeelsLike: current.feels_like,
+      currentHumidity: current.humidity,
+      currentWindSpeed: current.wind_speed,
+      currentDescription: current.weather[0].main,
+      currentDetails: current.weather[0].description,
+    };
+
+    const hourlyWeather = await getWeatherArrayHourly(hourly);
+
+    const dailyWeather = await getWeatherArrayDaily(daily);
 
     const allWeather = {
       currentWeather,
