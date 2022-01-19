d75d5cc34f30ad1bbadfcaadecd96996073d9ddf
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-01-19 17:31:13 -0500

    get current location based on browser

---
+++ src/functions/getCurrentLocation.js
@@ -0,0 +1,25 @@
+function getCurrentPositionPromise() {
+  // eslint-disable-next-line no-promise-executor-return
+  return new Promise((resolve, reject) => navigator
+    .geolocation.getCurrentPosition(resolve, reject));
+}
+
+async function returnCoordinates() {
+  try {
+    const location = await getCurrentPositionPromise();
+    const lat = location.coords.latitude;
+    const lon = location.coords.longitude;
+    return { lat, lon };
+  } catch (error) {
+    // eslint-disable-next-line no-alert
+    return alert(
+      `There was an error getting your location. 
+      We will now default to Chicago.
+
+      ${error.code}: ${error.message}
+      `,
+    );
+  }
+}
+
+export default returnCoordinates;
