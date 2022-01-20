d3637a7cef8bbc2a415f44a266948cca72b027a8
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-01-19 21:16:42 -0500

    set states

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import returnCoordinates from './functions/getCurrentLocation';
-import { getCurrentCityWeather, getCurrentCoordinatesWeather } from './functions/getWeather';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  async function defaultLoad() {
+// Import components
+import Loading from './components/Loading';
+import Header from './components/Header';
+import CurrentWeather from './components/CurrentWeather';
+import HourlyWeather from './components/HourlyWeather';
+import DailyWeather from './components/DailyWeather';
+import Footer from './components/Footer';
+
+// Import functions
+import returnCoordinates from './functions/getCurrentLocation';
+import { getCityCoordinates, getCurrentCoordinatesWeather } from './functions/getWeather';
+import emptyWeatherArray from './functions/emptyWeatherArray.json';
+
+export default function App() {
+  const [weather, setWeather] = useState(emptyWeatherArray);
+  const [units, setUnits] = useState('imperial');
+
+  async function getWeatherAtCurrentLocation() {
     try {
-      const { lat, lon } = await returnCoordinates();
-      const browserLocationWeather = await getCurrentCoordinatesWeather(lat, lon, 'metric');
-      console.log(browserLocationWeather);
-    } catch {
-      const chicagoDefaultWeather = await getCurrentCityWeather('Chicago', 'metric');
-      console.log(chicagoDefaultWeather);
+      const currentCoordinates = await returnCoordinates();
+      setWeather(await getCurrentCoordinatesWeather(
+        currentCoordinates.lat,
+        currentCoordinates.lon,
+        units,
+      ));
+    } catch (error) {
+      console.warn(error);
     }
   }
 
-  defaultLoad();
+  useState(() => getWeatherAtCurrentLocation(), []);
 
   return (
-    <div className="app-container">Test</div>
+    <div className="app-container">
+      {weather === emptyWeatherArray ? <Loading /> : ''}
+      <Header />
+      <CurrentWeather
+        item={weather.currentWeather}
+      />
+      <HourlyWeather />
+      <DailyWeather />
+      <Footer />
+    </div>
   );
 }
-
-export default App;
