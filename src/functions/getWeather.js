1f633c4c39eaffa1145a5b643b7555f0cda63c36
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-01-19 21:16:27 -0500

    delete getWeather by city function, not useful

--- src/functions/getWeather.js
+++ src/functions/getWeather.js
@@ -1,6 +1,6 @@
 const APIKey = process.env.REACT_APP_API_KEY;
 
-async function getCurrentCity(city) {
+export async function getCityCoordinates(city) {
   try {
     const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`);
     const data = await response.json();
@@ -21,6 +21,7 @@ async function getWeatherArrayHourly(obj) {
       date: new Date(obj[i].dt * 1000).getHours(),
       temperature: obj[i].temp,
       description: obj[i].weather[0].main,
+      icon: obj[i].weather[0].icon,
     };
     weatherArray.push(arrayItem);
   }
@@ -34,46 +35,11 @@ async function getWeatherArrayDaily(obj) {
       date: new Date(obj[i].dt * 1000).getDay(),
       minTemperature: obj[i].temp.min,
       maxTemperature: obj[i].temp.max,
+      icon: obj[i].weather[0].icon,
     };
     weatherArray.push(arrayItem);
   }
   return weatherArray;
-}
-
-export async function getCurrentCityWeather(city, units) {
-  try {
-    const currentCityCoord = await getCurrentCity(city);
-    const { lat, lon } = currentCityCoord;
-
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&exclude=minutely&appid=${APIKey}`);
-    const data = await response.json();
-
-    const { current, hourly, daily } = data;
-
-    const currentWeather = {
-      currentTime: new Date(current.dt * 1000),
-      currentTemp: current.temp,
-      currentFeelsLike: current.feels_like,
-      currentHumidity: current.humidity,
-      currentWindSpeed: current.wind_speed,
-      currentDescription: current.weather.main,
-      currentDetails: current.weather.description,
-    };
-
-    const hourlyWeather = await getWeatherArrayHourly(hourly);
-
-    const dailyWeather = await getWeatherArrayDaily(daily);
-
-    const allWeather = {
-      currentWeather,
-      hourlyWeather,
-      dailyWeather,
-    };
-
-    return allWeather;
-  } catch (error) {
-    return console.log(error);
-  }
 }
 
 export async function getCurrentCoordinatesWeather(lat, lon, units) {
