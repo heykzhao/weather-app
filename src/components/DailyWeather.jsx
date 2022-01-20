bb4e586094b87cd41054e1c05e7bef62431423e5
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-01-19 21:15:40 -0500

    create blank components

---
+++ src/components/DailyWeather.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function DailyWeather() {
+  return (
+    <div>Daily Weather</div>
+  );
+}
---
+++ src/components/HourlyWeather.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function HourlyWeather() {
+  return (
+    <div>Hourly Weather</div>
+  );
+}
