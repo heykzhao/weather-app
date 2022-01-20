ed63e3932aac6f0f5e05656803be89eae298dedc
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-01-20 15:17:52 -0500

    Add icon, basic HTML structure for all data

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-export default function CurrentWeather({ item }) {
+export default function CurrentWeather({ item, city }) {
   const {
     currentDescription,
     currentDetails,
@@ -11,10 +13,33 @@ export default function CurrentWeather({ item }) {
     currentTemp,
     currentTime,
     currentWindSpeed,
+    currentIcon,
   } = item;
 
-  const time = new Date(currentTime);
+  const date = new Date(currentTime);
+  const dayYearMonthDate = date.toLocaleString('en-us', {
+    weekday: 'long', year: 'numeric', month: 'long', date: 'numeric',
+  });
+  const time = date.toLocaleTimeString([], {
+    hour12: true, hour: '2-digit', minute: '2-digit',
+  });
+
+  const iconSource = `http://openweathermap.org/img/wn/${currentIcon}@2x.png`;
+
   return (
-    <div className="current-weather-container">    </div>
+    <div className="current-weather-container">
+      <div className="current-time">Last updated at: {dayYearMonthDate} at {time}</div>
+      <div className="city-name">{city}</div>
+      <div className="cw-temperature">{currentTemp}</div>
+      <div className="cw-feels-like">{currentFeelsLike}</div>
+      <div className="cw-description">{currentDescription}</div>
+      <div className="cw-details">{currentDetails}</div>
+      <div className="cw-humidity">{currentHumidity}</div>
+      <div className="cw-wind-speed">{currentWindSpeed}</div>
+      <img
+        alt="Current weather icon"
+        src={iconSource}
+      />
+    </div>
   );
 }
