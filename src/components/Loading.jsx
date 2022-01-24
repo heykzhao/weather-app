717f653abe9045b47deb7ba95f734cf7616ed3d0
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-01-24 15:45:27 -0600

    Incoporate loading image

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import '../styles/Loading.css';
 
 export default function Loading() {
   return (
-    <div>Loading</div>
+    <div className="loader">
+      <div className="loader-inner">
+        <div className="loader-line-wrap">
+          <div className="loader-line" />
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line" />
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line" />
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line" />
+        </div>
+        <div className="loader-line-wrap">
+          <div className="loader-line" />
+        </div>
+      </div>
+    </div>
   );
 }
