6830f78f80f5242c6e5274b8225c9d1d94be6949
heykzhao <72359861+heykzhao@users.noreply.github.com>
2022-01-24 15:45:03 -0600

    Incorporate footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
+import '../styles/Footer.css';
+import githubLogo from '../images/GitHub-Mark.png';
+
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="nameplate noselect">
+      <span className="copyright">
+        Made by
+        <a href="https://github.com/heykzhao" target="_blank" rel="noreferrer"> heykzhao</a>
+      </span>
+      <span>
+        <a href="https://github.com/heykzhao" target="_blank" rel="noreferrer">
+          <img
+            alt="Github Logo"
+            src={githubLogo}
+            className="github-logo"
+          />
+        </a>
+      </span>
+    </div>
   );
 }
